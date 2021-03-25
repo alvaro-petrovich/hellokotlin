ef22e51b18500a0766eb60b0f13e331ea81e5a04
Alvaro Petrovich Cid Contreras <alvaro.petrovich@gmail>
2021-03-25 13:21:47 -0600

    Toast

--- app/src/main/java/mx/edu/ittehuacan/hellokotlin/register/RegisterActivity.kt
+++ app/src/main/java/mx/edu/ittehuacan/hellokotlin/register/RegisterActivity.kt
@@ -5,13 +5,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import mx.edu.ittehuacan.hellokotlin.BaseActivity
 import mx.edu.ittehuacan.hellokotlin.R
+import mx.edu.ittehuacan.hellokotlin.databinding.ActivityRegisterBinding
 
 class RegisterActivity : BaseActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_register)
+    //setContentView(R.layout.activity_register)
+    //ActivityLoginBinding.inflate( layoutInflater ).apply { setContentView( root ) }
+    val reference = ActivityRegisterBinding.inflate( layoutInflater )
+    setContentView( reference.root )
+    reference.textView3
+    reference.textInputName
+    reference.textInputLastName
+    //ActivityMainBinding
+    //ActivityLoginBinding
   }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -21,6 +31,7 @@ class RegisterActivity : BaseActivity() {
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
     if ( item.itemId == R.id.save_register ) {
+      Toast.makeText(this, getString(R.string.toast_save_register), Toast.LENGTH_SHORT).show()
       finish()
       return true
     }
